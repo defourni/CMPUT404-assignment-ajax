6b1f832561cf2809ce9bbc80f1afd567a8ea361d
Dexter Fournier <defourni@cs.ualberta.ca>
2020-03-06 11:34:03 -0700

    added license

--- server.py
+++ server.py
@@ -20,6 +20,29 @@
 # remember to:
 #     pip install flask
 
+#/***************************************************************************************
+#*    Title: ObserverExampleAJAX
+#*    Author: Hazel Campbell, Abram Hindle
+#*    Date: March 6, 2019
+#*    Availability: https://github.com/uofa-cmput404/cmput404-slides/tree/master/examples/ObserverExampleAJAX
+#*
+#***************************************************************************************/
+
+
+#Copyright 2020 Dexter Fournier
+
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+
+    #http://www.apache.org/licenses/LICENSE-2.0
+
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+
 
 import flask
 from flask import Flask, request, render_template, redirect
